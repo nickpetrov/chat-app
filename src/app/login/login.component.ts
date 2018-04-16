7711d0380bf81c751da406766cecf227dc591aeb
Nick Pe <springate.nick@gmail.com>
2018-04-16 18:11:55 +0300

    Implements user registration

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import * as firebase from 'firebase/app';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/take';
+
 import { FlashMessagesService } from 'angular2-flash-messages';
 
 import { AuthService } from '../auth.service';
@@ -17,8 +22,9 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private _auth: AuthService,
-    private _router: Router,
-    private _flashMessagesService: FlashMessagesService
+    private _afs: AngularFirestore,
+    private _flashMessagesService: FlashMessagesService,
+    private _router: Router
   ) {
     this.loginData = {
       email: '',
@@ -29,8 +35,21 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  private createUser(user: firebase.User) {
+    return this._afs.collection('users', ref => ref.where('userid', '==', user.uid))
+      .valueChanges()
+      .take(1)
+      .toPromise()
+      .then(res => {
+        if (!res.length) {
+          this._afs.collection('users').add({ uid: user.uid, email: user.email, displayName: user.displayName });
+        }
+      });
+  }
+
   public login = (f: NgForm) => this._auth.login()
-    .then(success => this._router.navigate(['/chat']))
+    .then((user: any) => this.createUser(user.user))
+    .then(_ => this._router.navigate(['/chat']))
     .catch(error => this._flashMessagesService.show(error, { cssClass: 'alert-error', timeout: 3000 }));
 
   public loginInWithEmailAndPassword = () => this._auth.loginInWithEmailAndPassword(this.loginData)
@@ -38,7 +57,8 @@ export class LoginComponent implements OnInit {
     .catch(error => {
       this._flashMessagesService.show(error, { cssClass: 'alert-error', timeout: 3000 });
       this._auth.createUserWithEmailAndPassword(this.loginData)
-        .then(success => this._router.navigate(['/chat']))
+        .then((user: firebase.User) => this.createUser(user))
+        .then(_ => this._router.navigate(['/chat']))
         .catch(error => {
           this._flashMessagesService.show(error, { cssClass: 'alert-error', timeout: 3000 });
         });
